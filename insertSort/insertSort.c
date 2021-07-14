f29e36df18d36589ec1eaee11ead233a9d28a0dd
Xieji Li <1808028940@qq.com>
2021-07-14 20:14:28 +0800

    insertSort(IO,DO)

--- insertSort/insertSort.c
+++ insertSort/insertSort.c
@@ -2,8 +2,8 @@
 #include<stdlib.h>
 
 
-void insertSort(int* array,int size){
-	for(int pointer1 = 0;pointer1 < size;pointer1 ++){
+void IO_insertSort(int* array,int size){
+	for(int pointer1 = 1;pointer1 < size;pointer1 ++){
 		int key = array[pointer1];
 		int pointer2 = pointer1 - 1;
 		while(pointer2 >=  0 && array[pointer2] > key){
@@ -14,11 +14,25 @@ void insertSort(int* array,int size){
 	}
 }
 
+void DO_insertSort(int* array,int size){
+	for(int pointer1 = 1;pointer1 < size; pointer1 ++){
+		int key = array[pointer1];
+		int pointer2 = pointer1 - 1;
+		while(pointer2 >= 0 && array[pointer2] < key){
+			array[pointer2 + 1] = array[pointer2];
+			pointer2 --;
+		}
+		array[pointer2+1] = key;
+	}	
+}
+
 void display(int* array,int size){
 	for(int i = 0; i < size; i++){
 		printf("%d\n",array[i]);
 	}
 }
+
+
 
 int size(int* array){
 	return sizeof(array)+1;
--- insertSort/main.c
+++ insertSort/main.c
@@ -3,7 +3,7 @@
 int main(){
 	int array[]= {19,2,3,4,1,2,5,6,4};
 	display(array,size(array));
-	insertSort(array,size(array));
+	DO_insertSort(array,size(array));
 	printf("---\n");
 	display(array,size(array));
 }
