c6ae70369c558539fe181123a107bc632ed73caf
sisuy <1808028940@qq.com>
2022-03-15 15:23:05 +0800

    add random partitian

--- Quik sort/QuickSort/src/main.java
+++ Quik sort/QuickSort/src/main.java
@@ -2,10 +2,10 @@ import QuickSort.QuickSort;
 
 class Test{
     public static void main(String[] args){
-        int[] array = {5,1,2,14,5,67,2};
-        QuickSort.printArray(array);
+        int[] array = {2,1,4,1,5,6,2,22,1,3};
 
-        QuickSort.quickSort(array,0,6);
-        QuickSort.printArray(array);
+        QuickSort.printArray(array); //print array  elements
+        QuickSort.quickSort(array,0,array.length - 1);// quick sort
+        QuickSort.printArray(array);//print results
     }
 }
